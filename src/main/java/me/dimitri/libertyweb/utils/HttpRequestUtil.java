3cecd16168849798effe7d51c7bba5505787ce4e
KoxSosen <67807644+KoxSosen@users.noreply.github.com>
2024-03-12 14:42:35 +0100

    Make the HttRequests async.

--- src/main/java/me/dimitri/libertyweb/utils/HttpRequestUtil.java
+++ src/main/java/me/dimitri/libertyweb/utils/HttpRequestUtil.java
@@ -8,6 +8,11 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Singleton
 public class HttpRequestUtil {
@@ -22,12 +27,23 @@ public class HttpRequestUtil {
     public String get(String url) {
         try {
             URI uri = URI.create(url);
-            HttpRequest request = HttpRequest.newBuilder().uri(uri).GET().build();
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                return response.body();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .version(HttpClient.Version.HTTP_2)
+                    .timeout(Duration.ofMinutes(2))
+                    .setHeader("User-Agent", "LibertyWeb Data")
+                    .GET()
+                    .uri(uri)
+                    .build();
+
+            CompletableFuture<HttpResponse<String>> response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+            int statusCode = response.thenApply(HttpResponse::statusCode).get(5, TimeUnit.SECONDS);
+            String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+
+            if (statusCode == 200) {
+                return result;
             }
-        } catch (IOException | InterruptedException e) {
+
+        } catch (InterruptedException | ExecutionException | TimeoutException e) {
             throw new RuntimeException(e);
         }
         return null;
